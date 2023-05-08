a6f1dbf692e7c2c83cda36a7b02cf2945cc73059
Tanner-Denti <tannerwdenti@gmail.com>
2023-05-07 22:05:29 -0600

    Add regular mode drawing

--- script.js
+++ script.js
@@ -5,6 +5,48 @@ function removeAllChildNodes(parent) {
     }
 }
 
+// Adds the proper number of "pixel" divs based on a value
+function populateSketchContainer(value) {
+    const sketchContainer = document.querySelector(".sketch-container");
+    removeAllChildNodes(sketchContainer);
+
+    for (let i = 0; i < value; i++)
+    {
+        for (let j = 0; j < value; j++) 
+        {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.style.width = `${500/value}px`;
+            pixel.style.height = `${500/value}px`;
+            pixel.style.boxSizing = 'border-box';
+            pixel.style.backgroundColor = 'white';
+            
+            sketchContainer.appendChild(pixel);
+        }
+    }
+}
+
+function mark(event) {
+    event.target.style.backgroundColor = '#3c3c41'
+}
+
+function startDrawing() {
+    let pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', mark);
+    });
+}
+
+function stopDrawing() {
+    let pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        pixel.removeEventListener('mouseover', mark);
+    });
+}
+
+// Make sure the page loads with "pixels" in the Etch-a-Sketch
+populateSketchContainer(document.querySelector('.slider').value)
+
 // Slider
 document.querySelector(".slider").oninput = function() {
     let value = (this.value-this.min)/(this.max-this.min)*100;
@@ -13,25 +55,10 @@ document.querySelector(".slider").oninput = function() {
     value = (value > 1) ? Math.ceil(value) : 1;
     document.querySelector(".slider-label").innerText = `${value} x ${value}`;
 
-    const sketchContainer = document.querySelector(".sketch-container");
-    removeAllChildNodes(sketchContainer)
+    populateSketchContainer(value);
+};
 
-    for (let i = 0; i < value; i++)
-    {
-        for (let j = 0; j < value; j++) 
-        {
-            const pixel = document.createElement('div');
-            pixel.classList.add('pixel');
-            pixel.style.width = `${500/value}px`
-            pixel.style.height = `${500/value}px`
-            pixel.style.boxSizing = 'border-box'
-            pixel.style.borderStyle = 'solid'
-            pixel.style.border = '0.1px solid black'
-            pixel.style.backgroundClip = 'white'
-            
-            sketchContainer.appendChild(pixel)
-        }
-    }
-  };
-
+// Allow / disallow drawing
+document.body.onmousedown = () => (startDrawing());
+document.body.onmouseup = () => (stopDrawing());
 
